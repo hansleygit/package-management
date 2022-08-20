32cd9e3efe2d776fbdb6e49b8d9a19ddaa33078d
hansleygit <98280548+hansleygit@users.noreply.github.com>
2022-08-20 14:47:45 -0400

    Update docker-Jenkins-installation-ubuntu.sh

--- docker-Jenkins-installation-ubuntu.sh
+++ docker-Jenkins-installation-ubuntu.sh
@@ -23,4 +23,4 @@ sudo echo "jenkins  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/jenkins
 # sudo echo "jenkins:admin" | chpasswd
 # sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
 # sudo service sshd restart
-# sudo usermod -aG  docker jenkins
+
