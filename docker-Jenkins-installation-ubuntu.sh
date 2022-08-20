b7f3379cff30ff45eebffbab769ff8107a0aa7b4
hansleygit <98280548+hansleygit@users.noreply.github.com>
2022-08-20 01:52:06 -0400

    Update docker-Jenkins-installation-ubuntu.sh

--- docker-Jenkins-installation-ubuntu.sh
+++ docker-Jenkins-installation-ubuntu.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # Run this as a script
-sudo hostname docker
+sudo apt update -y
+# sudo hostname docker
 sudo apt install docker.io -y
-sudo usermod -aG docker ubuntu 
+sudo systemctl start docker
+sudo systemctl enable docker.service
+# Install start and Configure Jenkins
 # Install java as jenkins dependency
 sudo apt install openjdk-11-jdk -y
 # install jenkis in ubuntu:
@@ -12,9 +15,12 @@ sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
 sudo apt-get update
 sudo apt-get install jenkins -y 
 sudo systemctl start jenkins 
-
-echo "jenkins  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/jenkins" 
-sudo echo "jenkins:admin" | chpasswd
-sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
-sudo service sshd restart
-sudo usermod -aG  docker jenkins
+# Add jenkins user to docker group and restart docker
+sudo usermod -aG docker jenkins
+sudo systemctl restart docker.service
+# Add jenkins user to the sudoers group 
+sudo echo "jenkins  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/jenkins 
+# sudo echo "jenkins:admin" | chpasswd
+# sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
+# sudo service sshd restart
+# sudo usermod -aG  docker jenkins
